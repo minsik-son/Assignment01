21ef51b53574e9c8c2fec5e251b675e275ed4612
Minsik Son <minsik.son@gmail.com>
2025-05-03 13:12:27 -0700

    Second

--- src/Date.java
+++ src/Date.java
@@ -1,4 +1,4 @@
-public class Date implements Orderable {
+public class Date implements Comparable<Date>, Orderable<Date> {
     private int day;
     private int month;
     private int year;
@@ -15,5 +15,56 @@ public class Date implements Orderable {
 
     public String toString(){
         return year + "-" + month + "-" + day;
+    }
+
+    public int checkLastDate(int year, int month){
+        int lastDate;
+        switch (month){
+            case 1:
+                lastDate = 31;
+                break;
+            case 2:
+                lastDate = 28;
+                break;
+            case 3:
+                lastDate = 31;
+                break;
+            case 4:
+                lastDate = 30;
+                break;
+            case 5:
+                lastDate = 31;
+                break;
+            case 6:
+                lastDate = 30;
+                break;
+            case 7:
+                lastDate = 31;
+                break;
+            case 8:
+                lastDate = 31;
+                break;
+            case 9:
+                lastDate = 30;
+                break;
+            case 10:
+                lastDate = 31;
+                break;
+            case 11:
+                lastDate = 30;
+                break;
+            case 12:
+                lastDate = 31;
+                break;
+        }
+        return lastDate;
+    }
+
+    public Orderable next(){
+        if(day)
+    }
+
+    public Orderable previous(){
+
     }
 }
